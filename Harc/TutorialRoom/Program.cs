843c9d3692919d889ab019ae1ee4186dc7bbea1b
Kovacs Zoltan <hidan075@gmail.com>
2023-02-21 17:22:26 +0100

    szobak generelasa es harcrendszer demo

---
+++ Harc/TutorialRoom/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(200, 45);
+
+            Console.SetCursorPosition(75, 25);
+
+            Console.WriteLine(@"Statok:
+                                                                            Életerő: 60
+                                                                            Erő: 4
+                                                                            Józanság: 50
+                                                                            Védekezés: 6
+                                                                            Energia: 5");
+
+            Console.SetCursorPosition(72, 0);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"|
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(88, 0);
+
+            Console.WriteLine("Eszköztár");
+
+            Console.SetCursorPosition(87, 1);
+
+            Console.WriteLine("fa bot s:2");
+
+            Console.SetCursorPosition(87, 2);
+
+            Console.WriteLine("Kötött sapka v:2 *");
+
+            Console.SetCursorPosition(75, 20);
+
+            Console.WriteLine("? megvizsgál | + használ | - eldob");
+
+            Console.SetCursorPosition(70,7);
+
+            Console.WriteLine(@" 
+                                    
+                            \    /
+                             \__/
+                           _| .. |_
+                          |   __   |
+                          \__ vv __/
+                          /_|    |_\
+                          V_|\__/|_V
+                    ");
+
+
+
+            Console.SetCursorPosition(0, 23);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"_________________________________________________________________________________________________________________");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(1, 25);
+
+            Console.WriteLine(@"Vérnyúl Életerő: 20
+Ellenség lépése: sebzés 6
+Mi a következő lépésed?
+
+
+ +
+Mit használsz föl?
+ 1
+Wow támadásod 8 sebzést mért az ellenfélre!");
+
+
+        }
+    }
+}
---
+++ Szobak/TutorialRoom/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(200, 45);
+
+            Console.SetCursorPosition(75, 25);
+
+            Console.WriteLine(@"Statok:
+                                                                            Életerő: 80
+                                                                            Erő: 3
+                                                                            Józanság: 70
+                                                                            Védekezés: 6
+                                                                            Energia: 2");
+
+            Console.SetCursorPosition(72, 0);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"|
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |
+                                                                        |");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(88, 0);
+
+            Console.WriteLine("Eszköztár");
+
+            Console.SetCursorPosition(75, 20);
+
+            Console.WriteLine("? megvizsgál | + használ | - eldob");
+
+            Console.SetCursorPosition(2, 13);
+
+            Console.WriteLine(@"__
+ (`/\
+ `=\/\ __...--~~~~~-._   _.-~~~~~--...__
+  `=\/\               \ /               \\
+   `=\/                V                 \\
+   //_\___--~~~~~~-._  |  _.-~~~~~~--...__\\
+  //  ) (..----~~~~._\ | /_.~~~~----.....__\\
+ ===( INK )==========\\|//====================
+__ejm\___/________dwb`---`____________________________________________");
+
+
+            Console.SetCursorPosition(0, 23);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(@"_________________________________________________________________________________________________________________");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(1, 25);
+
+            Console.WriteLine(@"A következő három ötlet jut eszedbe
+
+ >> Egy poros pince ahol egy csontvár (nagyon veszélyes) ül a sarokban.
+ >> Körbenézel a falak homokból vannak és egy szarkofág (? veszélyes)
+    van előtted.
+ >> A falak ki vannak matracozva és egy megkötözött ember (?) 
+    van a szobába rajtad kívül.");
+
+        }
+    }
+}
