af07f1f87a87953f85a535b9353e6645323cb425
SzokolAttila <szokol.ati2005@gmail.com>
2024-04-24 12:49:35 +0200

    Added NotSecurePasswordException.cs

---
+++ Agoraphobia/AgoraphobiaLibrary/Exceptions/NotSecurePasswordException.cs
@@ -0,0 +1,12 @@
+﻿namespace AgoraphobiaLibrary.Exceptions
+{
+    public class NotSecurePasswordException : Exception
+    {
+        public NotSecurePasswordException() : base(
+            "Your password needs to:\n\tbe at least 8 characters long,\n\tcontain a special character,\n\t" +
+            "contain a lower case letter,\n\tcontain an upper case letter,\n\tcontain a number!")
+        {
+            
+        }
+    }
+}
