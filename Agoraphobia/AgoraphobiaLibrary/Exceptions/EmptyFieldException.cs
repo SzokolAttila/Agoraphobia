708320b4bbd2aede8ef6c4b0360920c83de114e3
SzokolAttila <szokol.ati2005@gmail.com>
2024-04-24 12:38:19 +0200

    added EmptyFieldException.cs and created Exceptions folder

---
+++ Agoraphobia/AgoraphobiaLibrary/Exceptions/EmptyFieldException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgoraphobiaLibrary.Exceptions
+{
+    public class EmptyFieldException : Exception
+    {
+        public EmptyFieldException()
+            : base("A required field has been left empty!")
+        {
+
+        }
+    }
+}
