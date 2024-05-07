b3500894e1c9d510811e2758d1d208d6668640c1
SzokolAttila <szokol.ati2005@gmail.com>
2024-05-07 12:31:16 +0200

    added a rudimentary frame for minimal API

---
+++ Agoraphobia/AgoraphobiaAPI/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Agoraphobia API", Description = "It's high time you got started with that book...", Version = "v1" });
+});
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "Agoraphobia API V1");
+   });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
