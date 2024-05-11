5a829a0d02e37130b066ce52b448364635ed7883
SzokolAttila <szokol.ati2005@gmail.com>
2024-05-11 15:54:37 +0200

    added tooLongUsernameException, modified TooShortUsernameException so that it receives length from constructor

---
+++ Agoraphobia/AgoraphobiaLibrary/Exceptions/TooLongUsernameException.cs
@@ -0,0 +1,10 @@
+﻿namespace AgoraphobiaLibrary.Exceptions;
+
+public class TooLongUsernameException : Exception
+{
+    public TooLongUsernameException(int lengthLimit)
+        : base($"The maximum character length for a username is {lengthLimit}!")
+    {
+        
+    }
+}
--- Agoraphobia/AgoraphobiaLibrary/Exceptions/TooShortUsernameException.cs
+++ Agoraphobia/AgoraphobiaLibrary/Exceptions/TooShortUsernameException.cs
@@ -2,10 +2,9 @@
 
 public class TooShortUsernameException : Exception
 {
-    public TooShortUsernameException()
-        : base($"Username needs to be at least {MINIMUM_LENGTH} characters long!")
+    public TooShortUsernameException(int minimumLength)
+        : base($"Username needs to be at least {minimumLength} characters long!")
     {
         
     }
-    public const int MINIMUM_LENGTH = 6;
 }
