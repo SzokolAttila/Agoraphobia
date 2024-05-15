c1ca17e8c1c7ed648a4d03814da1c51eb9c28e8c
SzokolAttila <szokol.ati2005@gmail.com>
2024-05-15 18:25:59 +0200

    added base values for player stats

--- Agoraphobia/AgoraphobiaLibrary/Player.cs
+++ Agoraphobia/AgoraphobiaLibrary/Player.cs
@@ -7,6 +7,15 @@ public class Player
     public Player(int accountId)
     {
         AccountId = accountId;
+        Weapons = new List<Weapon>();
+        Health = BASE_HEALTH;
+        MaxHealth = BASE_HEALTH;
+        Energy = BASE_ENERGY;
+        MaxEnergy = BASE_ENERGY;
+        Sanity = BASE_SANITY;
+        Defense = BASE_DEFENSE;
+        Attack = BASE_ATTACK;
+        DreamCoins = BASE_DREAMCOINS;
     }
     [JsonConstructor]
     public Player(int id, int accountId, double sanity, double maxHealth, double health, int maxEnergy, int energy, double attack, double defense, int dreamCoins, List<Weapon> weapons)
@@ -34,4 +43,10 @@ public class Player
     public double Defense { get; set; }
     public int DreamCoins { get; set; }
     public List<Weapon> Weapons { get; set; }
+    private const double BASE_SANITY = 30;
+    private const double BASE_HEALTH = 42;
+    private const int BASE_ENERGY = 3;
+    private const double BASE_ATTACK = 4.5;
+    private const double BASE_DEFENSE = 5.2;
+    private const int BASE_DREAMCOINS = 100;
 }
